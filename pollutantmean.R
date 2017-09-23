8f6abade59c6bf9dc0ffba115e444c40847b359d
Aurel Prosz <aurel.prosz@gmail.com>
2017-09-23 17:32:34 +0200

    fixed
    fixed

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,23 +1,24 @@
 pollutantmean <- function(directory, pollutant, id) {
   mean_temp <- 0
+  setwd(directory)
   id_file <- sprintf("%03d", id)
   j <- 1
   for(i in id){
-    setwd(directory)
     dir <- paste(id_file[j], "csv", sep=".")
+    print(dir)
     all <- read.csv2(dir, header = TRUE, sep= ",")
     filtered <- all[[pollutant]]
     bad <- is.na(filtered)
     good <- filtered[!bad]
     numeric_good <- as.numeric(levels(good))[good]
-    mean_temp <- c(mean_temp, (mean(numeric_good)))
+    mean_temp <- c(mean_temp, (numeric_good))
     j <- j + 1
     
   }
-  all <- mean_temp[-1]
-  bad <- is.na(all)
-  good <- all[!bad]
   
-  return(mean(good))
+  all <- mean_temp[-1]
+  
+  
+  return(mean(all))
   
 }
