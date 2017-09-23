fbeb0c62324a1ade3765f25dd7191a04f6355783
Aurel Prosz <aurel.prosz@gmail.com>
2017-09-23 17:55:31 +0200

    up

--- pollutantmean.R
+++ pollutantmean.R
@@ -6,8 +6,6 @@ pollutantmean <- function(directory, pollutant, id) {
   for(i in id){
     dir <- paste(id_file[j], "csv", sep=".")
     dir <- paste(directory, dir, sep= "/")
-    print(directory)
-    print(dir)
     all <- read.csv2(dir, header = TRUE, sep= ",")
     filtered <- all[[pollutant]]
     bad <- is.na(filtered)
@@ -22,5 +20,34 @@ pollutantmean <- function(directory, pollutant, id) {
   
   
   return(mean(all))
+  
+}
+
+complete <- function(directory,id){
+  id_num <- 0
+  length <- 0
+  directory <- paste(getwd(), directory,sep= "/")
+  id_file <- sprintf("%03d", id)
+  j <- 1
+  for(i in id){
+    dir <- paste(id_file[j], "csv", sep=".")
+    dir <- paste(directory, dir, sep= "/")
+    all <- read.csv2(dir, header = TRUE, sep= ",")
+    filtered <- complete.cases(all)
+    good <- all[filtered,]
+    id_num <- c(id_num,i)
+    length <- c(length, nrow(good))
+    
+    j <- j + 1
+    
+  }
+  
+  id_num <- id_num[-1]
+  length <- length[-1]
+  
+  output <- data.frame(id_num, length)
+  
+  return(output)
+  
   
 }
